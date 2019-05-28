9044d8dccdeebebd80637ad5caf417c24a20e601
Yujin Chung <yujin.mar.chung@gmail.com>
2019-05-29 06:19:54 +0900

    Wed29May JavaStudy

---
+++ JavaStudy/java_tutorials/src/marofmaar/javatutorials/variables/StringDemo.java
@@ -0,0 +1,13 @@
+package marofmaar.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first = "coding";
+		System.out.println(first + " everyday");
+		
+	
+
+	}
+
+}
---
+++ JavaStudy/src/marofmar/jajavava/variables/IntegerDemo.java
@@ -0,0 +1,12 @@
+package marofmar.jajavava.variables;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a = 1;
+		System.out.println(a+1); //2
+
+	}
+
+}
