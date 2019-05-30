af21c3c8d14fff9a409ed0fcb5e5eaa887579a50
Yujin Chung <yujin.mar.chung@gmail.com>
2019-05-30 23:06:19 +0900

    TodayJava

---
+++ JavaStudy/java_tutorials/src/marofmaar/javatutorials/variables/CodingEveryBody.java
@@ -0,0 +1,13 @@
+package marofmaar.javatutorials.variables;
+
+public class CodingEveryBody {
+
+	public static void main(String[] args) {
+		int a = 100;
+		System.out.println(a+10);
+		System.out.println((a+10)/10);
+		System.out.println(((a+10)/10)-10);
+
+	}
+
+}
