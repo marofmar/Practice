93769dc80bb3d53bfbe0f7203bb8d48227ba6b0a
Yujin Chung <yujin.mar.chung@gmail.com>
2019-05-31 18:00:12 +0900

    TIL Fri31May2019

--- JavaStudy/java_tutorials/src/marofmaar/javatutorials/variables/CodingEveryBody.java
+++ JavaStudy/java_tutorials/src/marofmaar/javatutorials/variables/CodingEveryBody.java
@@ -7,6 +7,19 @@ public class CodingEveryBody {
 		System.out.println(a+10);
 		System.out.println((a+10)/10);
 		System.out.println(((a+10)/10)-10);
+		// wow, single line comment
+		
+		/*
+		 this is for
+		 multiple 
+		 comments
+		 */
+		
+		
+		/**
+		 * ohhhhhh
+		 * JAVA DOC comments
+		 */
 
 	}
 
