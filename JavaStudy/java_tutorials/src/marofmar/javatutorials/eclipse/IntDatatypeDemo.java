4884416a2b3353b69c76098353924b295ed3e174
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-04 17:01:52 +0900

    TIL_JAVA

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/eclipse/IntDatatypeDemo.java
@@ -0,0 +1,17 @@
+package marofmar.javatutorials.eclipse;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		short a; 
+		a = 128;
+		int b;
+		b = 123445899;
+		double c;
+		c = 1.3;
+		
+		
+
+	}
+
+}
