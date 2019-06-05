a541b0fa0a93f33f938247215971a2ef042cca95
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-05 10:35:52 +0900

    TIL_java_190605

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/operatorrss/arithmaticsDemo.java
@@ -0,0 +1,27 @@
+package marofmar.javatutorials.operatorrss;
+
+public class arithmaticsDemo {
+
+	public static void main(String[] args) {
+		// result 값은 3
+		int result = 1+2;
+		System.out.println(result);
+		// result 값은 2
+		result = result -1;
+		System.out.println(result);
+		// result 값은 4
+		result = result *2;
+		System.out.println(result);
+		// result 값은 2
+		result = result /2;
+		System.out.println(result);
+		// result 값은 10
+		result = result + 8;
+		System.out.println(result);
+		// result 값은 3
+		result = result % 7;
+		System.out.println(result);
+
+	}
+
+}
---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/operatorrss/concateDemo.java
@@ -0,0 +1,14 @@
+package marofmar.javatutorials.operatorrss;
+
+public class concateDemo {
+
+	public static void main(String[] args) {
+		String firstString = "This is";
+		String secondString = " a concatenated one.";
+		String thirdString = firstString + secondString;
+		System.out.println(thirdString);
+				
+
+	}
+
+}
