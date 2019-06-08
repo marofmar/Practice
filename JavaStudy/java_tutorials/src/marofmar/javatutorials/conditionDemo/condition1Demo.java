7493cec931a96208b4218a50403a0e3dd33eb9c6
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-08 17:59:03 +0900

    TIL_JAVA_Sat8Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/conditionDemo/condition1Demo.java
@@ -0,0 +1,12 @@
+package marofmar.javatutorials.conditionDemo;
+
+public class condition1Demo {
+
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println("reuslt: true");
+		}
+
+	}
+
+}
