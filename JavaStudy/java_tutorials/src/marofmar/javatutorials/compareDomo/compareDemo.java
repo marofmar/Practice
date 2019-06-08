3fc67f95595b784f99bae5bb9c56fe0995b42c00
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-08 17:49:52 +0900

    TIL_Java_Sat8Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/compareDomo/compareDemo.java
@@ -0,0 +1,19 @@
+package marofmar.javatutorials.compareDomo;
+
+public class compareDemo {
+
+	public static void main(String[] args) {
+		System.out.println(1==2);
+		System.out.println(1==1);
+		System.out.println(1!=2);
+		System.out.println(10>20);
+		System.out.println(10>=10);
+		
+		String a = "Hello World";
+		String b = new String("Hello world");
+		System.out.println(a ==b);
+		System.out.println(a.equals(b));
+
+	}
+
+}
