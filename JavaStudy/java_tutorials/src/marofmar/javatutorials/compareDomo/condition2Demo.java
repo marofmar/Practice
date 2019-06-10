585e4a737f5a44c7bd8d89690c481f2c52594717
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-10 17:37:33 +0900

    TIL_Java_Mon10Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/compareDomo/condition2Demo.java
@@ -0,0 +1,15 @@
+package marofmar.javatutorials.compareDomo;
+
+public class condition2Demo {
+
+	public static void main(String[] args) {
+		if (false) {
+			System.out.println(1);
+		} 
+		else {
+			System.out.println(2);
+		}
+
+	}
+
+}
