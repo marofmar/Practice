d3874bd80702669232b28545ab7fb9483267abb2
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-15 22:48:08 +0900

    TIL_java_or_sat15jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javaatutorials/conditionaalOperators/orDemo.java
@@ -0,0 +1,25 @@
+package marofmar.javaatutorials.conditionaalOperators;
+
+public class orDemo {
+
+	public static void main(String[] args) {
+		if (true||true) {
+			System.out.println(1);
+		}
+		
+		if (true||false) {
+			System.out.println(2);
+		
+		}
+		
+		if (false||true) {
+			System.out.println(3);
+		}
+		
+		if (false||false) {
+			System.out.println("FALSEEE");
+		}
+
+	}
+
+}
