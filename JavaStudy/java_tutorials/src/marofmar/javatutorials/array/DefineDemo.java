2f1d44443c456ab0e31cc6bfcb9f65673c781ef9
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-23 15:35:51 +0900

    TIL_Java_arrayCarolYujin

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/array/DefineDemo.java
@@ -0,0 +1,12 @@
+package marofmar.javatutorials.array;
+
+public class DefineDemo {
+
+	public static void main(String[] args) {
+		String[] classGroup = {"Carol","Yujin"};
+		System.out.println(classGroup[0]);
+		System.out.println(classGroup[1]);
+
+	}
+
+}
