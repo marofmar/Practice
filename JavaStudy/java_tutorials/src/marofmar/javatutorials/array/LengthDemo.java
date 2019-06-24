42e4c0aaa036c84bb4dc3ae03ed1be2378c4dafe
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-24 13:06:02 +0900

    TIL_Java_Mon24Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/array/LengthDemo.java
@@ -0,0 +1,16 @@
+package marofmar.javatutorials.array;
+
+public class LengthDemo {
+
+	public static void main(String[] args) {
+		String[] classGroup = new String[4];
+		classGroup[0] = "Carol";
+		System.out.println(classGroup.length);
+		classGroup[1] = "Yujin";
+		classGroup[2] = "Bryson";
+		System.out.println(classGroup.length);
+		classGroup[3] = "Alex";
+
+	}
+
+}
