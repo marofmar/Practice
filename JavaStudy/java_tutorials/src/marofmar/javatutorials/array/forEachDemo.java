3d7a768cd02f67ce3060bc73c22fde39209954a9
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-25 19:21:44 +0900

    TIL_Java_Tue25Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/array/forEachDemo.java
@@ -0,0 +1,13 @@
+package marofmar.javatutorials.array;
+
+public class forEachDemo {
+
+	public static void main(String[] args) {
+		String[] members = {"Carol","Bryson","Yujin"};
+		for (String e : members) {
+			System.out.println(e + " has ordered a coffee!");
+		}
+
+	}
+
+}
