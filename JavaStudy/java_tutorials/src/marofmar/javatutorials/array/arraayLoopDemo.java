5a0b07bdc84117ff6f7f9cb9407b122080f8017a
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-25 19:17:10 +0900

    TIL_Java_Tue25Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/array/arraayLoopDemo.java
@@ -0,0 +1,14 @@
+package marofmar.javatutorials.array;
+
+public class arraayLoopDemo {
+
+	public static void main(String[] args) {
+		String[] members = {"Carol","Bryson","Yujin"};
+		for (int i = 0; i < members.length; i++) {
+			String member = members[i];
+			System.out.println(member + "is happy!");
+		}
+
+	}
+
+}
