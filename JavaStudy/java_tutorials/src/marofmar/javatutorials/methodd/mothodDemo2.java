40d8e69200dd44bae492019670ec63d8b679b577
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-26 15:51:06 +0900

    TIL_Java_Wed26Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/methodd/mothodDemo2.java
@@ -0,0 +1,18 @@
+package marofmar.javatutorials.methodd;
+
+public class mothodDemo2 {
+	
+	public static void numbering(int limit) {
+		int i = 0;
+		while (i<limit) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		numbering(4);
+
+	}
+
+}
