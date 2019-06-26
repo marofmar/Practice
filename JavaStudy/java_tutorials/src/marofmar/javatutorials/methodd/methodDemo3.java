25e23d995f4c926a5eb8fb400ae0f8be4616e022
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-26 15:55:57 +0900

    TIL_Java_Wed26Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/methodd/methodDemo3.java
@@ -0,0 +1,18 @@
+package marofmar.javatutorials.methodd;
+
+public class methodDemo3 {
+	
+	public static void numbering(int init, int limit) {
+		int i = init;
+		while (i < limit) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		numbering(1,9);
+
+	}
+
+}
