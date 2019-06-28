3ece82e5ef505f8ee6775874f4812c5f6ac18837
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-28 20:52:47 +0900

    TIL_Java_Fri28Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/methodd/methodDemo5.java
@@ -0,0 +1,29 @@
+package marofmar.javatutorials.methodd;
+
+import java.io.*;
+
+public class methodDemo5 {
+	public static String numbering(int init, int limit) {
+		int i = init;
+		String output = "";
+		while (i < limit) {
+			output += i;
+			i++;
+		}
+		return output;
+	}
+
+	public static void main(String[] args) {
+		String result = numbering(1, 5);
+		System.out.println(result);
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
+			out.write(result);
+			out.close();
+		} catch (IOException e) { 
+			
+		}
+
+	}
+
+}
