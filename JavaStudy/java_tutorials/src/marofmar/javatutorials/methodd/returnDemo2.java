0a1d765b0bec376825eb538216f470e30b954a3a
Yujin Chung <yujin.mar.chung@gmail.com>
2019-06-29 19:34:56 +0900

    TIL_Java_Sat29Jun

---
+++ JavaStudy/java_tutorials/src/marofmar/javatutorials/methodd/returnDemo2.java
@@ -0,0 +1,20 @@
+package marofmar.javatutorials.methodd;
+
+public class returnDemo2 {
+	public static String num(int i) {
+		if (i == 0) {
+			return "zero";
+		} else if (i == 1) {
+			return "one";
+		} else if (i ==2) {
+			return "two";
+		}
+		return "none";
+	}
+
+	public static void main(String[] args) {
+		System.out.println(num(1));
+
+	}
+
+}
