e64b051e8c63119295dfcd70a5d683d986731cbe
Yujin Chung <yujin.mar.chung@gmail.com>
2019-07-01 17:42:23 +0900

    TIL_Java_Mon1Jul

---
+++ JavaStudy/java_tutorials/src/java_tutorials/eclip/HeyWorld.java
@@ -0,0 +1,10 @@
+package java_tutorials.eclip;
+
+public class HeyWorld {
+
+	public static void main(String[] args) {
+		System.out.println(10+20);
+
+	}
+
+}
---
+++ JavaStudy/java_tutorials/src/java_tutorials/object/CalculatorDemo.java
@@ -0,0 +1,12 @@
+package java_tutorials.object;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		// Assume super complex code
+		System.out.println(10 + 20);
+		System.out.println(20+40);
+
+	}
+
+}
